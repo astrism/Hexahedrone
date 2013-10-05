c2d4e58309420a948cb5312e2ec4e45db7dc306f
Grant H <ghelton@gmail.com>
2013-10-04 19:27:15 -0700

    added all vars to scope, added pause var

--- js/sim.js
+++ js/sim.js
@@ -9,21 +9,25 @@ hexahedrone.directive('sim', function() {
 				'js/Box',
 				'js/BasicBox'
 				],
-				function () {
-					//loaded and can be used here now.
-					$scope.game = new Phaser.Game(800, 
-												600, 
-												Phaser.AUTO, 
-												'sim', 
-												{ 
-													preload: preload, 
-													create: create, 
-													update: update,
-													render: render 
-												}
-											);
-				}
+				initGame
 			);
+
+
+			function initGame() {
+				//loaded and can be used here now.
+				$scope.game = new Phaser.Game(800, 
+											600, 
+											Phaser.AUTO, 
+											'sim', 
+											{ 
+												preload: preload, 
+												create: create, 
+												update: update,
+												render: render 
+											}
+										);
+				$scope.$watch('game.paused', onPauseChange);
+			}
 
 			// Constants
 			var SPRITE_FLOOR = 'floor';
@@ -33,11 +37,12 @@ hexahedrone.directive('sim', function() {
 			}
 
 			// objects
-			var gameOver = false;
-			var boxA;
-			var boxB;
-			var boxAWins = 0;
-			var boxBWins = 0;
+			$scope.paused = false;
+			$scope.gameOver = false;
+			$scope.boxA;
+			$scope.boxB;
+			$scope.boxAWins = 0;
+			$scope.boxBWins = 0;
 
 			function create() {
 				Parse.initialize(CONFIG.PARSE_APP_ID, CONFIG.PARSE_JS_KEY);
@@ -46,49 +51,49 @@ hexahedrone.directive('sim', function() {
 				$scope.game.stage.scale = 0.5;
 
 				var yPos = $scope.game.world.height - 100;
-				boxA = new BasicBox('A', 200, yPos);
-				boxA.createWithGame($scope.game);
+				$scope.boxA = new BasicBox('A', 200, yPos);
+				$scope.boxA.createWithGame($scope.game);
 
-				boxB = new BasicBox('B', $scope.game.world.width - 300, yPos);
-				boxB.createWithGame($scope.game);
+				$scope.boxB = new BasicBox('B', $scope.game.world.width - 300, yPos);
+				$scope.boxB.createWithGame($scope.game);
 
 				// fued
-				boxA.setTarget(boxB);
-				boxB.setTarget(boxA);
+				$scope.boxA.setTarget($scope.boxB);
+				$scope.boxB.setTarget($scope.boxA);
 			}
 
 			function update() {
 				var gt = $scope.game.time.time
-				if(!gameOver) {
+				if(!$scope.gameOver) {
 					// fighter collisions
-					$scope.game.physics.collide(boxA.sprite, boxB.sprite, onCollide);
-					boxA.update(gt);
-					if(boxA.currentAction)
-						document.getElementById('boxAAttack').innerHTML = 'move: ' + boxA.currentAction.name;
-					document.getElementById('boxAHealth').innerHTML = 'health: ' + Math.round(boxA.health);
-					boxB.update(gt);
-					if(boxB.currentAction)
-						document.getElementById('boxBAttack').innerHTML = 'move: ' + boxB.currentAction.name;
-					document.getElementById('boxBHealth').innerHTML = 'health: ' + Math.round(boxB.health);
+					$scope.game.physics.collide($scope.boxA.sprite, $scope.boxB.sprite, onCollide);
+					$scope.boxA.update(gt);
+					if($scope.boxA.currentAction)
+						document.getElementById('boxAAttack').innerHTML = 'move: ' + $scope.boxA.currentAction.name;
+					document.getElementById('boxAHealth').innerHTML = 'health: ' + Math.round($scope.boxA.health);
+					$scope.boxB.update(gt);
+					if($scope.boxB.currentAction)
+						document.getElementById('boxBAttack').innerHTML = 'move: ' + $scope.boxB.currentAction.name;
+					document.getElementById('boxBHealth').innerHTML = 'health: ' + Math.round($scope.boxB.health);
 				} else {
-					$scope.game.physics.collide(boxA.sprite, boxB.sprite);
+					$scope.game.physics.collide($scope.boxA.sprite, $scope.boxB.sprite);
 				}
 			}
 
 			function render() {
 
-				// $scope.game.debug.renderRectangle(boxA.sprite.body);
-				// $scope.game.debug.renderRectangle(boxB.sprite.body);
+				// $scope.game.debug.renderRectangle($scope.boxA.sprite.body);
+				// $scope.game.debug.renderRectangle($scope.boxB.sprite.body);
 
 			}
 
 			function onCollide(spriteA, spriteB) {
-				if(!gameOver) {
+				if(!$scope.gameOver) {
 					var velA = Math.abs(spriteA.velocity.x);
 					var velB = Math.abs(spriteB.velocity.x);
 					// console.log('a:', Math.round(velA), 'b:', Math.round(velB));
 
-					if(Math.abs(velA - velB) > 100) {
+					if(Math.abs(velA - velB) > 0) {
 					// console.log('spriteA.velocity.x:', Math.abs(spriteA.velocity.x));
 					// console.log('spriteB.velocity.x:', Math.abs(spriteB.velocity.x));
 					// console.log('veldiff:', Math.abs(velA - velB));
@@ -96,44 +101,44 @@ hexahedrone.directive('sim', function() {
 						// injured object is moving faster due to collision?
 						if(velA < velB) {
 							// console.log('injure B');
-							if(boxA.sprite.bottomLeft.y < boxB.sprite.topLeft.y) {
+							if($scope.boxA.sprite.bottomLeft.y < $scope.boxB.sprite.topLeft.y) {
 								console.log('head stomp on B');
-								boxA.rebound();
-								boxB.stomp(15);
+								$scope.boxA.rebound();
+								$scope.boxB.stomp(15);
 							}
 							console.log('injure B');
-							boxB.injure(5);
-							boxA.charge();
+							$scope.boxB.injure(5);
+							$scope.boxA.charge();
 						} else {
 							// console.log('injure A');
-							if(boxB.sprite.bottomLeft.y < boxA.sprite.topLeft.y) {
+							if($scope.boxB.sprite.bottomLeft.y < $scope.boxA.sprite.topLeft.y) {
 								console.log('head stomp on A');
-								boxB.rebound();
-								boxA.stomp(15);
+								$scope.boxB.rebound();
+								$scope.boxA.stomp(15);
 							}
 							console.log('injure A');
-							boxA.injure(5);
-							boxB.charge();
+							$scope.boxA.injure(5);
+							$scope.boxB.charge();
 						}
 
 						// death check
-						if(boxA.dead || boxB.dead) {
+						if($scope.boxA.dead || $scope.boxB.dead) {
 							console.log('game over');
 
 							// notify box
-							boxA.endGame(boxB.dead);
-							boxB.endGame(boxA.dead);
+							$scope.boxA.endGame($scope.boxB.dead);
+							$scope.boxB.endGame($scope.boxA.dead);
 
 							//record win
-							if(boxB.dead) {
-								boxAWins++;
-								document.getElementById('boxAWins').innerHTML = 'wins: ' + boxAWins;
+							if($scope.boxB.dead) {
+								$scope.boxAWins++;
+								document.getElementById('boxAWins').innerHTML = 'wins: ' + $scope.boxAWins;
 							}
-							if(boxA.dead) {
-								boxBWins++;
-								document.getElementById('boxBWins').innerHTML = 'wins: ' + boxBWins;
+							if($scope.boxA.dead) {
+								$scope.boxAWins++;
+								document.getElementById('boxBWins').innerHTML = 'wins: ' + $scope.boxAWins;
 							}
-							gameOver = true;
+							$scope.gameOver = true;
 
 							// timer for next battle
 							setTimeout(startNewBattle, 2500);
@@ -143,15 +148,19 @@ hexahedrone.directive('sim', function() {
 			}
 
 			function startNewBattle() {
-				boxA.restart();
-				boxB.restart();
+				$scope.boxA.restart();
+				$scope.boxB.restart();
 
-				gameOver = false;
+				$scope.gameOver = false;
 			}
 
 
 			$scope.togglePause = function() {
 				$scope.game.paused = !$scope.game.paused;
+			}
+
+			function onPauseChange(newValue, oldValue){
+				$scope.paused = newValue;
 			}
 		}
 	}
