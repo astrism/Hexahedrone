b6e4e57fa24192989f0a5e3f7bbb7b01e090c58d
Grant H <ghelton@gmail.com>
2013-10-14 23:24:37 -0700

    changed default page centered grid text

--- js/routes.js
+++ js/routes.js
@@ -11,7 +11,7 @@ define([
 		function($stateProvider, $urlRouterProvider) {
 
 		// For any unmatched url, redirect to /state1
-		$urlRouterProvider.otherwise("/sim");
+		$urlRouterProvider.otherwise("/stats");
 
 		// $stateProvider.state('home', {
 		// 	url: "/",
